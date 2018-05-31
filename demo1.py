36742698e610c738d5b771087517ef9397ba2597
rpSOLV <roger.lpt17@gmail.com>
2018-05-31 13:08:26 -0400

    Slides, demos, and references.

---
+++ demo1.py
@@ -0,0 +1,58 @@
+import numpy as np
+from scipy.stats import norm
+from time import sleep
+
+np.random.seed(1) # For reproducibility
+
+"""
+   BEGIN INPUT
+"""
+
+p = 0.5 # 1e-5       # Failure Probability
+n = int(1e6)         # Number of samples
+delta = 0.05         # Confidence parameter
+replicas = int(1e4)  # Experiment replicas
+
+"""
+   END INPUT
+"""
+
+z = norm.ppf(1-delta/2)  # Z-score based on confidence level (1-delta)
+
+def experiment(p, n, z):
+    """ Test validity of CLT based confidence inervals for the mean of a Bernoulli random variable.
+    
+    Args:
+        p: Success probability.
+        n: Number of samples.
+        z: Z-score for probability tails.
+    
+    Returns:
+        True if confidence bound covers p, Fals otherwise.
+    
+    """
+    
+    # Sample mean and variance
+    bmu = np.random.binomial(n, p)*1.0/n
+    bvar = bmu * (1-bmu) * n/(n-1)
+    
+    # Cofidence Interval
+    ci = [bmu - z*np.sqrt(bvar/n), bmu + z*np.sqrt(bvar/n)]
+    
+    # Test coverage
+    return p >= ci[0] and p <= ci[1]
+
+# Run Experiment 10 times more
+
+confidence = []
+print('Confidence (1-delta) in practice:\n')
+for trial in range(10):
+    
+    # Replicas
+    confidence.append(np.mean([experiment(p, n, z) for i in range(replicas)]))
+    print('Trial %d: %.3f' % (trial+1, confidence[-1]) + (' Overconfident!' if confidence[-1]<(1-delta) else ''))
+    
+    sleep(0.5)
+
+    
+print('\nAggregating all trials: %.3f ' % (np.mean(confidence)))
---
+++ readme.rst
@@ -0,0 +1,41 @@
+************************************************************
+PAC-Algorithms for Reliability Estimation of Complex Systems
+************************************************************
+
+:Conference: Engineering Mechanics Institute Conference 2018
+:Location: Boston, MA. USA.
+:Date: May 31, 2018
+:Authors: Roger Paredes
+:Contact: roger(dot)paredes(at)rice.edu
+:Web site: http://github.com/paredesroger/emi2018
+
+
+Note
+----
+
+This repository contains slides, demos, and references shown at the conference presentation.
+
+
+Demos
+-----
+
+To download the demos simply clone the repository: ::
+
+  $ git clone https://github.com/paredesroger/emi2018
+
+Then, run examples issuing: ::
+
+  $ python demo1.py
+
+and
+
+  $ python demo2.py
+
+
+References
+----------
+
+The main results are based in the following two papers:
+
+- Duenas-Osorio, L., Meel, K. S., Paredes, R., & Vardi, M. Y. (2017). Counting-based Reliability Estimation for Power-Transmission Grids. In Proceedings of AAAI Conference on Artificial Intelligence. San Francisco.
+- Paredes, R., Duenas-Osorio, L., Meel, K. S., & Vardi, M. Y. (2018). Network Reliability Estimation in Theory and Practice. Submitted to: Reliability Engineering & System Safety. (Preprint will be made available soon)
